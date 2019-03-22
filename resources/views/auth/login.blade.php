fbb16d42840bd26eb0b69eb8eeca4f0a5cdf8d3b
kibmuikia <muikiaa@gmail.com>
2019-03-22 16:11:29 +0300

    ui modification continuation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,21 +5,27 @@
 <div class="container jazaHeight">
     <div class="section">
         <div class="row">
-            <div class="col s12">
+            <div class="col s12 m6 offset-m3">
                 <div class="card">
                     <div class="card-content">
-                        <span class="card-title">Login</span>
                         <div class="row">
-                            <form class="col s12" method="POST" action="{{ route('login') }}">
+                            <div class="col s6 offset-s3 m6 offset-m3">
+                                <span class="card-title">Login</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <form class="col s12 m12" method="POST"
+                            action="{{ route('login') }}">
                                 @csrf
                                 <div class="row">
-                                    <div class="input-field col s12 m6 offset-m3">
-                                        <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required autofocus>
+                                    <div class="input-field col s12 m12">
+                                        <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required >
+                                        <!-- autofocus -->
                                         <label for="email">
                                           {{ __('E-Mail Address') }}
                                         </label>
                                         @if ($errors->has('email'))
-                                            <span class="" role="alert">
+                                            <span class="red-text text-lighten-1" role="">
                                                 <strong>{{ $errors->first('email') }}</strong>
                                             </span>
                                         @endif
@@ -31,7 +37,7 @@
                                             {{ __('Password') }}
                                         </label>
                                         @if ($errors->has('password'))
-                                            <span class="" role="">
+                                            <span class="red-text text-lighten-1" role="">
                                                 <strong>{{ $errors->first('password') }}</strong>
                                             </span>
                                         @endif
@@ -45,13 +51,13 @@
                                     </div>
                                     <div class="input-field col s12 m12">
                                         <div class="row">
-                                            <div class="col s12 m6">
+                                            <div class="col s6 m4">
                                                 <button type="submit"
                                                 class="waves-effect waves-light btn">
                                                     {{ __('Login') }}
                                                 </button>
                                             </div>
-                                            <div class="col s12 m6">
+                                            <div class="col s12 m8">
                                                 @if (Route::has('password.request'))
                                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                                         {{ __('Forgot Your Password?') }}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,82 @@
-@extends('layouts.app')
+@extends('layouts.app2')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+<div class="container jazaHeight">
+    <div class="section">
+        <div class="row">
+            <div class="col s12 m6 offset-m3">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="row">
+                            <div class="col s6 offset-s3 m6 offset-m3">
+                                <span class="card-title">Register</span>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="row">
+                            <form method="POST" action="{{ route('register') }}"
+                            class="col s12 m12">
+                                @csrf
+                                <div class="row">
+                                    <div class="input-field col s12 m12">
+                                        <input type="text" name="name" id="name"
+                                        class="validate" required  value="{{ old('name') }}"/>
+                                        <label for="name">
+                                            {{ __('Name') }}
+                                        </label>
+                                        @if ($errors->has('name'))
+                                            <span class="red-text text-lighten-1">
+                                                <!-- role="alert" -->
+                                                <strong>{{ $errors->first('name') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="input-field col s12 m12">
+                                        <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required >
+                                        <!-- autofocus -->
+                                        <label for="email">
+                                          {{ __('E-Mail Address') }}
+                                        </label>
+                                        @if ($errors->has('email'))
+                                            <span class="red-text text-lighten-1" role="">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="input-field col s12 m12">
+                                        <input id="password" type="password" class="validate"
+                                        name="password" required>
+                                        <label for="password">
+                                            {{ __('Password') }}
+                                        </label>
+                                        @if ($errors->has('password'))
+                                            <span class="red-text text-lighten-1" role="">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="input-field col s12 m12">
+                                        <input id="password-confirm" type="password"
+                                        class="validate"
+                                        name="password_confirmation" required>
+                                        <label for="password-confirm">
+                                            {{ __('Confirm Password') }}
+                                        </label>
+                                    </div>
+                                    <div class="input-field col s12 m12">
+                                        <button type="submit"
+                                        class="waves-effect waves-light btn">
+                                            {{ __('Register') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
